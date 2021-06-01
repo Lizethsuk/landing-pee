817891ffc9e1383877cc44a89c38808f8febcc76
sandra <sandrarocio12oc@gmail.com>
2021-06-01 17:11:57 -0500

    add components

---
+++ src/components/CursosPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="cursos-page">
+      <b-container class="bv-example-row section-areas">
+        <b-row>
+          <b-col>
+            <p v-text="title"></p>
+            <ul class="navbar-nav d-inline-block d-flex flex-wrap">
+             <slot></slot>
+          
+            </ul> 
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CursosPage",
+  props: {
+    title: {
+      type: String,
+      require: true,
+    }
+  },
+ 
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../styles/main.styl';
+
+.cursos-page {
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+}
+
+.navbar-nav {
+  flex-direction: row;
+}
+
+.nav-link {
+  padding-left: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  align-items: flex-end;
+}
+
+
+.section-areas {
+  padding: 50px 0px;
+  padding-top: 20px;
+}
+
+section {
+  border-bottom: 4px solid primary-pee;
+}
+</style>
