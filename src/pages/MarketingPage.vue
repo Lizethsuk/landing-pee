f1fa918743082dd16ea83fdc250028879950f32b
sandra <sandrarocio12oc@gmail.com>
2021-06-28 18:23:20 -0500

    add test

--- src/pages/MarketingPage.vue
+++ src/pages/MarketingPage.vue
@@ -80,9 +80,9 @@ export default {
     CursoList,
     PasosPages,
   },
-  // beforeCreate() {
-  //   window.scrollTo(0, 0);
-  // },
+  beforeCreate() {
+    window.scrollTo(0, 0);
+  },
   data() {
     return {
       myJson: json,
